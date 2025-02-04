6208eb5bd0668caf8d12fd51ab9339a46e0cc76f
mennareda568 <mennaghalwash12@gmail.com>
2025-02-04 05:57:02 +0200

    Add files via upload

---
+++ Admin/password.php
@@ -0,0 +1,61 @@
+<?php
+include("initials.php");
+session_start();
+if (isset($_SESSION['login'])) {
+
+    $page = "All";
+    if ($page == "All") {
+        $statment1 = $connect->prepare("SELECT * FROM users");
+        $statment1->execute();
+        $result = $statment1->fetchall();
+    }
+    if (isset($_GET['page'])) {
+        $page = $_GET['page'];
+    }
+
+    if (isset($_GET['user_id'])) {
+        $user_id = $_GET['user_id'];
+    }
+
+    $statment1 = $connect->prepare("SELECT * FROM users WHERE	user_id =?");
+    $statment1->execute(array($user_id));
+    $item = $statment1->fetch();
+
+?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 mt-5">
+                <form action="?page=password" method="post">
+                    <input type="hidden" name="old_id" value="<?php echo $item['user_id']; ?>" class="form-control mb-3 ">
+                    <input type="password" name="pass" class="form-control mb-3">
+                    <input type="submit" value="Update" class="form-control mt-3 btn btn-success ">
+                </form>
+            </div>
+        </div>
+    </div>
+<?php
+    if ($page == "password") {
+
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+            $old_id = $_POST['old_id'];
+            $pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+            try {
+                $statment = $connect->prepare("UPDATE USERS SET 
+           `password`=?,
+            updated_at=now() 
+            WHERE user_id=?");
+                $statment->execute(array($pass, $old_id));
+                $_SESSION['message'] = "UPDATED SUCESSFULLY";
+                header("Location:users.php");
+            } catch (PDOException $e) {
+                $_SESSION['message'] = "Error: " . $e->getMessage();
+                header("Location:users.php");
+            }
+        }
+    }
+} else {
+    $_SESSION['message'] = "Please Login First";
+    header("Location:../login.php");
+}
+include("Includes/temp/footer.php");
+?>
