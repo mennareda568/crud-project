00a35061a0e69e505ddbdf7012d9acdb80f88637
mennareda568 <mennaghalwash12@gmail.com>
2025-02-05 00:43:28 +0200

    Update navbar.php

--- includes/temp/navbar.php
+++ includes/temp/navbar.php
@@ -1,18 +1,12 @@
 <header class="header-area header-sticky wow slideInDown" data-wow-duration="0.75s" data-wow-delay="0s">
     <div class="container">
         <div class="row">
-            <div class="col-12">
-                <nav class="main-nav">
-                    <!-- ***** Logo Start ***** -->
-                    <a href="home.php" class="logo">
-                    </a>
-                    <!-- ***** Logo End ***** -->
+            <div class="col-10">
+                <nav class="main-nav">                  
                     <!-- ***** Menu Start ***** -->
                     <ul class="nav">
                         <li><a href="home.php" class="active">Home</a></li>
-                        <li><a href="#">Contact Us</a></li>
-                        <li><a href="#">Category</a></li>
-                        <li><a href="#">Listing</a></li>
+                        <li><a href="home.php">Contact Us</a></li>
                         <?php
                         echo '<div class="d-flex">
                                 <div class="main-white-button "><a href="login.php"> Login</a></div>' . '<div class="main-white-button mx-3"><a href="register.php"> Register</a></div>
@@ -20,9 +14,6 @@
                         ?>
                         </li>
                     </ul>
-                    <a class='menu-trigger'>
-                        <span>Menu</span>
-                    </a>
                     <!-- ***** Menu End ***** -->
                 </nav>
             </div>
