251cde446f02d58112817670102d164402761550
mennareda568 <mennaghalwash12@gmail.com>
2025-02-04 23:55:54 +0200

    Add files via upload

---
+++ Users/includes/temp/navbar.php
@@ -0,0 +1,28 @@
+<?php
+include("Users/includes/temp/header.php");
+session_start();
+?>
+<nav class="navbar navbar-expand-lg  bg-dark ">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Categories</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="posts.php">Posts</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="comments.php">Comments</a>
+      </li>
+      <li class="nav-item">
+        <a href="password.php?email=<?php echo $_SESSION['userlogin'] ?>" class="btn btn-warning">Change My Password </i></a>
+      </li>
+
+      <a class="nav-link btn btn-success ml-3" href="logout.php">Log out</a>
+      </li>
+    </ul>
+  </div>
+</nav>
