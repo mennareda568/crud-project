f8f1be13f7b52f00e83a39b7e852868a728feac4
mennareda568 <mennaghalwash12@gmail.com>
2025-02-07 10:31:18 +0200

    Update navbar.php

--- Admin/Includes/temp/navbar.php
+++ Admin/Includes/temp/navbar.php
@@ -1,26 +1,28 @@
 
 <?php
-include("Includes/temp/header.php");
+include("Authors/includes/temp/header.php");
 session_start();
 ?>
 <nav class="navbar navbar-expand-lg  bg-dark ">
-  <a class="navbar-brand" href="dashboard.php">DashBoard</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav ml-auto">
+      <?php
+      if (isset ($_SESSION['userlogin']) && isset($_SESSION['login'])){
+      ?>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Categories</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="posts.php">Articles</a>
+        </li>
+        <?php
+      }
+      ?>
       <li class="nav-item">
-        <a class="nav-link" href="users.php">Users</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="categories.php">Categories</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="posts.php">Articles</a>
-      </li>
-      <li class="nav-item">
-        <a href="password.php?email=<?php echo $_SESSION['login'] ?>" class="btn btn-warning">Change My Password </i></a>
+        <a href="password.php?email=<?php echo $_SESSION['userlogin'] ?>" class="btn btn-warning">Change My Password </i></a>
       </li>
 
       <a class="nav-link btn btn-success ml-3" href="logout.php">Log out</a>
