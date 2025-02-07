3662b4f2290e22b2a2ebd04a9b2a5db9c4d1709c
mennareda568 <mennaghalwash12@gmail.com>
2025-02-07 11:10:35 +0200

    Update navbar.php

--- Authors/includes/temp/navbar.php
+++ Authors/includes/temp/navbar.php
@@ -8,12 +8,18 @@ session_start();
   </button>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav ml-auto">
+      <?php
+      if (isset ($_SESSION['userlogin']) ){
+      ?>
         <li class="nav-item">
           <a class="nav-link" href="index.php">Categories</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="posts.php">Articles</a>
         </li>
+        <?php
+      }
+      ?>
       <li class="nav-item">
         <a href="password.php?email=<?php echo $_SESSION['userlogin'] ?>" class="btn btn-warning">Change My Password </i></a>
       </li>
